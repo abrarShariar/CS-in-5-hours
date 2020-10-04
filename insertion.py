7112a8fc2cf61346fbde5b0de555b31b39301d31
Abrar Shariar <shariarabrar@gmail.com>
2020-10-04 16:52:54 +0900

    insertion sort

---
+++ insertion.py
@@ -0,0 +1,20 @@
+def insertion_sort(num_list):
+    for i in range(1, len(num_list)):
+        prev_index = i - 1
+        current_index = i
+        while prev_index >= 0 and num_list[prev_index] > num_list[current_index]:
+            num_list[prev_index], num_list[current_index] = num_list[current_index], num_list[prev_index]
+            prev_index -= 1
+            current_index -= 1
+    
+    return num_list
+
+
+print(insertion_sort([10,9,1,4,5,0]))
+print(insertion_sort([10,9]))
+print(insertion_sort([1]))
+print(insertion_sort([]))
+
+
+
+
